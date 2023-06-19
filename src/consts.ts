1ffd9c1e376a627b29e1d067acba7f3a62a59ddd
Stephen Riesenberg <sjohnr@hotmail.com>
2023-06-18 19:41:46 -0500

    Moved church center URL to consts

--- src/consts.ts
+++ src/consts.ts
@@ -120,3 +120,4 @@ export const VIMEO_SHOWCASE_NAME = 'Messages 2023';
 export const YOUTUBE_CHANNEL_ID = 'UC7toQ0WRsc4n220h0QYeRHw';
 export const APP_STORE_URL = 'https://apps.apple.com/us/app/church-center-app/id1357742931';
 export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.ministrycentered.churchcenter&hl=en_US';
+export const CHURCH_CENTER_URL = 'https://stonybrookchurch.churchcenter.com';
