3fce891806f0280515e73577925fd5cfaf6647b7
Stephen Riesenberg <sjohnr@hotmail.com>
2024-02-04 21:39:20 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,22 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+const ignoredPaths = [
+  '/leaders',
+  '/staff',
+  '/give',
+  '/prayer',
+  '/connect',
+  '/special-events',
+  '/events/calendar'
+].map((path) => new RegExp(path));
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stonybrookchurch.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap({
+    filter: (page) => !ignoredPaths.some((ignoredPath) => ignoredPath.test(page))
+  })],
   build: {
     format: 'file',
     inlineStylesheets: 'always'
